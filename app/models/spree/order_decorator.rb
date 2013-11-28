68d66f0a71cf021b2abf72051704bbb4830736d6
Luis Doubrava <luis@cg.nl>
2013-11-28 12:47:26 +0100

    only store affiliate if blank

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -5,6 +5,8 @@ module Spree
 
     # add an affiliate id to the order
     def save_affiliate(affiliate_id)
+      return unless self.affiliate_id.blank?
+
       self.affiliate_id = affiliate_id
       self.save!
     end
